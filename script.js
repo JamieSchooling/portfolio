f337da1f219c52f95cba3cd8babeaad67ef1be0a
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2024-02-03 13:05:19 +0000

    Add About section

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+function elementInView(element) {
+    const elementTop = element.getBoundingClientRect().top;
+    return elementTop <= (window.innerHeight || document.documentElement.clientHeight);
+}
+
+function displayScrollElement(element) {
+    element.classList.add("scrolled");
+}
+function hideScrollElement(element) {
+    element.classList.remove("scrolled");
+}
+
+function handleScrollAnimation() {
+    scrollElements.forEach(element => {
+        if (elementInView(element)) {
+            displayScrollElement(element);
+        }
+        else {
+            hideScrollElement(element);
+        }
+    });
+}
+
+window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+});
+
