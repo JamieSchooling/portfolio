bc4059d24c94aa08e0ff693f6de0e4a7c167ec9a
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2024-02-04 18:47:08 +0000

    Add project section and image track logic

--- script.js
+++ script.js
@@ -34,3 +34,58 @@ window.addEventListener('scroll', () => {
     handleScrollAnimation();
 });
 
+const projectSection = document.querySelector(".project");
+const imageTrack = document.querySelector(".image-track");
+let mouseDownAt = 0;
+let percentage = 50;
+let lastPercentage = percentage;
+imageTrack.style.transform = `translate(${-percentage}%, 0%)`;
+updateImageParallax(percentage);
+projectSection.addEventListener("mousedown", e => {
+    mouseDownAt = e.clientX;
+});
+projectSection.addEventListener("touchstart", e => {
+    mouseDownAt = e.touches[0].pageX;
+});
+projectSection.addEventListener("mousemove", e => {
+    
+    if (mouseDownAt === 0) return;
+
+    const mouseDelta = mouseDownAt - e.clientX;
+    const maxDelta = window.innerWidth / 2;
+
+    percentage = (mouseDelta / maxDelta) * 100;
+    percentage += lastPercentage;
+    percentage = Math.max(percentage, 0);
+    percentage = Math.min(percentage, 100);
+    updateImageParallax(percentage);
+    imageTrack.style.transform = `translate(${-percentage}%, 0%)`;
+});
+projectSection.addEventListener("touchmove",  e => {    
+    if (mouseDownAt === 0) return;
+
+    const mouseDelta = mouseDownAt - e.touches[0].pageX;
+    const maxDelta = window.innerWidth * 2;
+
+    percentage = (mouseDelta / maxDelta) * 100;
+    percentage += lastPercentage;
+    percentage = Math.max(percentage, 0);
+    percentage = Math.min(percentage, 100);
+    updateImageParallax(percentage);
+    imageTrack.style.transform = `translate(${-percentage}%, 0%)`;
+});
+projectSection.addEventListener("mouseup", e => {
+    mouseDownAt = 0;
+    lastPercentage = percentage;
+});
+
+projectSection.addEventListener("touchend", e => {
+    mouseDownAt = 0;
+    lastPercentage = percentage;
+})
+
+function updateImageParallax(percentage) {
+    for (const image of imageTrack.getElementsByClassName("project-image")) {
+        image.style.objectPosition = `${percentage}%`
+    }
+}
